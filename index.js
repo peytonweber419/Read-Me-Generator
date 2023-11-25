f43dc58d132a286194fffd22d7d2b0f9d2ffd16e
Peyton Weber <weberpeyton11@gmail.com>
2023-11-25 14:39:41 -0600

    Initialized the application and fixed formatting errors.

---
+++ index.js
@@ -0,0 +1,59 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./Utils/generateMarkdown.js");
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'Give the name of your project.',
+  }, {
+    type: 'input',
+    name: 'description',
+    message: 'Give a description of your project.',
+  }, {
+    type: 'input',
+    name: 'installation',
+    message: 'How is your application installed?',
+  }, {
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use your application?',
+  }, {
+    type: 'input',
+    name: 'contribution',
+    message: 'Who are the contributors?',
+  }, {
+    type: 'input',
+    name: 'test',
+    message: 'How do you test your application?'
+  }, {
+    type: 'list',
+    message: 'Choose the following license.',
+    name: 'license',
+    choices: ['MIT', 'IBM', 'Apache', 'Mozilla'],
+  }, {
+    type: 'input',
+    name: 'username',
+    message: 'What is your GitHub username?'
+  }, {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?'
+  }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, generateMarkdown(data));
+}
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(data => {
+        writeToFile("./output/README.md", data)
+    })
+} 
+
+// Function call to initialize app
+init();
